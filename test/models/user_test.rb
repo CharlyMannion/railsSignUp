e31da0b6308cfa4061adb0c8ac7650c641a9a0aa
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-25 11:18:34 +0000

    the user valdidates the email, TDD'd

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @subject = User.new
+  end
+
+  test "email should be required" do
+    @subject.valid?
+
+    assert_includes(@subject.errors[:email], "can't be blank")
+  end
+
   # test "the truth" do
   #   assert true
   # end
 end
+
+# rake test test/models/user_test.rb
