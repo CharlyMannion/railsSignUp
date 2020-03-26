3134733302e7799a4e1fe3c980fda8c848165a4f
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-26 14:42:33 +0000

    refactors test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,11 +31,3 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include FactoryBot::Syntax::Methods
 end
-
-# ENV["RAILS_ENV"] ||= "test"
-# require File.expand_path("../../config/environment", __FILE__)
-# require "rails/test_help"
-#
-# class ActiveSupport::TestCase
-#   include FactoryBot::Syntax::Methods
-# end
