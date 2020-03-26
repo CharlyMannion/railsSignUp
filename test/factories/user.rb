d33185728c21f59f0028b91fc9e50a8935934f78
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-26 15:02:49 +0000

    fixes typo and gets faker gem call to work

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -2,10 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    # email Faker::Internet.email
-    # email Faker::Internet.safe_email
-    # password Faker::Internet.password
-    email { "sherlyn@example.org" }
-    password { "DdYaSjH8xW9wGhD" }
+    email { Faker::Internet.safe_email }
+    password { Faker::Internet.password }
   end
 end
