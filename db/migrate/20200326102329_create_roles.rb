837f4a838ed313716b49890c3701b397299968ce
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-26 11:31:02 +0000

    fixes second role validation by editing migration to database

--- db/migrate/20200326102329_create_roles.rb
+++ db/migrate/20200326102329_create_roles.rb
@@ -1,7 +1,7 @@
 class CreateRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :roles do |t|
-      t.string :name, unique: true, null: false
+      t.string :name, unique: true
 
       t.timestamps null: false
     end
--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  #
-  # def setup
-  #   @subject = Role.new
-  # end
+
+  def setup
+    @subject = Role.new
+  end
 
   context 'validations' do
-    # should validate_presence_of(:name)
-    # should validate_uniqueness_of(:name)
+    should validate_presence_of(:name)
+    should validate_uniqueness_of(:name)
   end
 end
